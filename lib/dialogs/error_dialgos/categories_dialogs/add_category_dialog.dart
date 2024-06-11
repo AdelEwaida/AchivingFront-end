cfe6e6d58b06fe0e375a2d5609d6c792737e3c17
Dania-Alqam <1180950@student.birzeit.edu>
2024-06-11 09:51:40 +0300

    Delete Dialog

--- lib/dialogs/error_dialgos/categories_dialogs/add_category_dialog.dart
+++ lib/dialogs/error_dialgos/categories_dialogs/add_category_dialog.dart
@@ -46,32 +46,36 @@ class _AdvanceSearchLogsDialogState extends State<AddCategoryDialog>
     final double dialogWidth = width * 0.3;
     final double dialogheight = height * 0.13;
     return AlertDialog(
-      backgroundColor: Colors.white,
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          const SizedBox(),
-          Text(_locale.addCategory),
-          Container(
-            decoration: const BoxDecoration(
-                shape: BoxShape.rectangle,
-                color: Color.fromARGB(255, 237, 34, 20)),
-            child: IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: const Icon(
-                  Icons.close_rounded,
-                  color: Colors.white,
-                )),
-          ),
-        ],
-      ),
-      content: SizedBox(
-        height: dialogheight,
+      backgroundColor: Colors.transparent,
+      contentPadding: EdgeInsets.zero,
+      content: Container(
+        color: Colors.white,
+        padding: const EdgeInsets.all(20),
         width: dialogWidth,
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const SizedBox(),
+                Text(_locale.addCategory),
+                Container(
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Color.fromARGB(255, 237, 34, 20)),
+                  child: IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(
+                        Icons.close_rounded,
+                        color: Colors.white,
+                      )),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
             CustomTextField2(
               text: Text(_locale.description),
               width: width * 0.25,
@@ -80,39 +84,36 @@ class _AdvanceSearchLogsDialogState extends State<AddCategoryDialog>
               onSubmitted: (text) {},
               onChanged: (value) {},
             ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  style: customButtonStyle1(Size(width * 0.08, height * 0.045),
+                      16, const Color(0xff1F6E8C)),
+                  child: Text(
+                    _locale.search,
+                    style: const TextStyle(color: whiteColor),
+                  ),
+                ),
+                SizedBox(
+                  width: width * 0.01,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: customButtonStyle1(
+                      Size(width * 0.1, height * 0.045), 16, redColor),
+                  child: Text(
+                    _locale.cancel,
+                    style: const TextStyle(color: whiteColor),
+                  ),
+                )
+              ],
+            ),
           ],
         ),
       ),
-      actions: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: ElevatedButton(
-                onPressed: () {},
-                style: customButtonStyle1(Size(width * 0.08, height * 0.045),
-                    16, const Color(0xff1F6E8C)),
-                child: Text(
-                  _locale.search,
-                  style: const TextStyle(color: whiteColor),
-                ),
-              ),
-            ),
-            SizedBox(
-              width: width * 0.01,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              style: customButtonStyle1(
-                  Size(width * 0.1, height * 0.045), 16, redColor),
-              child: Text(
-                _locale.cancel,
-                style: const TextStyle(color: whiteColor),
-              ),
-            )
-          ],
-        )
-      ],
     );
   }
 }
