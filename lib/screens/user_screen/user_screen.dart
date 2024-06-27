39e3eb90bbff11d4c19dd5322bebe3d172a9b067
Dania-Alqam <1180950@student.birzeit.edu>
2024-06-27 15:13:46 +0300

    users screen design

--- lib/screens/user_screen/user_screen.dart
+++ lib/screens/user_screen/user_screen.dart
@@ -68,7 +68,7 @@ class _UserScreenState extends State<UserScreen> {
               width: isDesktop ? width * 0.8 : width * 0.9,
               child: TableComponent(
                 key: UniqueKey(),
-                tableHeigt: height * 0.85,
+                tableHeigt: height * 0.81,
                 tableWidth: width * 0.85,
                 editPassword: chagnePassword,
                 delete: deleteUser,
